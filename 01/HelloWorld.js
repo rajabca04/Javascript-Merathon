7ac4bdd8a7f1493ab5900cb1b183e7534e50cd90
rajabca04 <rajabca201804@gmail.com>
2022-06-16 23:38:21 +0530

    first commit

---
+++ 01/HelloWorld.js
@@ -0,0 +1,28 @@
+console.log("Hello javascript merathon");
+
+// variable declaration
+
+let userName = "raja kumar"
+
+let userMobile = 99000900909
+let isLoggedIn = true
+
+console.log(userName );
+
+
+console.log("\u00E9");
+console.log("\u0301");
+
+// const café = 1;
+// const café = 2;
+
+// console.log(café);
+
+// let 
+// gameScore
+//  = 
+//  289;
+// console.log(gameScore);
+
+let score = 0
+; [score, score+3, score+5].forEach(console.log)
